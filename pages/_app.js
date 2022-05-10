219cc625cce73649560b2ca2618094215d37a98b
Jose Solorzano <jasolorzanoromero@gmail.com>
2022-05-10 00:25:22 -0500

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,12 @@ import Head from "next/head";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
+      {/* <Head>
         <meta
           httpEquiv="Content-Security-Policy"
           content="upgrade-insecure-requests"
         />
-      </Head>
+      </Head> */}
       <Component {...pageProps} />
     </>
   );
