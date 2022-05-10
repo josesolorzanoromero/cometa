1d19a4bf4a283df83b2f54ddef17065c33ee282a
Jose Solorzano <jasolorzanoromero@gmail.com>
2022-05-10 00:46:15 -0500

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        key: "Strict-Transport-Security",
+        value: "max-age=63072000; includeSubDomains; preload",
+      },
+    ];
+  },
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
