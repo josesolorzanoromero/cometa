d1db4c7cdf18682cdc93c601dd7e70685afa384a
Jose Solorzano <jasolorzanoromero@gmail.com>
2022-05-10 00:15:37 -0500

    first commit

--- utils/fetch.js
+++ utils/fetch.js
@@ -1,6 +1,4 @@
-const baseUrl = `http://ec2-3-239-221-74.compute-1.amazonaws.com:8000/api/v1`;
-const studentsInfoL = `students/3b35fb50-3d5e-41b3-96d6-c5566141fab0/`;
-const studentsOrders = `students/3b35fb50-3d5e-41b3-96d6-c5566141fab0/orders/`;
+const baseUrl = `https://ec2-3-239-221-74.compute-1.amazonaws.com:8000/api/v1`;
 export const getData = async (url, token) => {
   const res = await fetch(`${baseUrl}/${url}`, {
     method: "GET",
